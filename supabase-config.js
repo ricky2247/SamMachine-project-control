7d3a41ed847efa04e17b298a89b009002f6dcf76
R3za_am <RezaAhmadi.200418@gmail.com>
2026-09-20 15:15:56 +0330

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,12 +1,3 @@
-/*
-  Supabase configuration
-  ----------------------
-  Put your Supabase Project URL and Publishable/anon public key here.
-
-  IMPORTANT:
-  - Use only the public Publishable/anon key.
-  - NEVER put a service_role/secret key in this file.
-*/
 window.SUPABASE_CONFIG = {
   url: "https://mfjcudvqnaqpwghmgehk.supabase.co",
   anonKey: "sb_publishable_zHqs26M3tbLyj01hfLsRxw_uGzgZfp2"
